01562c42a5ff2e102d36c89bdee5b67bc79d14b3
leychay <ikhwan@mohamadikhwan.com>
2016-10-04 17:40:34 +0800

    typo Origin

--- class/api.class.php
+++ class/api.class.php
@@ -37,7 +37,7 @@ abstract class API
      */
     public function __construct($request)
     {
-        header("Access-Control-Allow-Orgin: *");
+        header("Access-Control-Allow-Origin: *");
         header("Access-Control-Allow-Methods: *");
         header("Content-Type: application/json");
 
